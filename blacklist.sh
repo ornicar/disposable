5e1ce4745cd78a97ded948944315785bb3fbd52e
Thibault Duplessis <t@lichess.org>
2026-05-03 11:00:17 +0200

    compile after adding to blacklist

--- blacklist.sh
+++ blacklist.sh
@@ -6,21 +6,18 @@ domain=$1
 if grep -q "$domain" domains.txt; then
   echo "$domain is already in domains.txt"
   exit 0
-else
-  # add new line and $domain to domains.txt
-  echo "" >>domains.txt
-  echo "$domain" >>domains.txt
-  echo "$domain added to domains.txt"
 fi
 
 # check if $domain is already in blacklist.txt
 if grep -q "$domain" blacklist.txt; then
   echo "$domain is already in blacklist.txt"
   exit 0
-else
-  echo "$domain" >>blacklist.txt
-  echo "$domain added to blacklist.txt"
 fi
+
+echo "$domain" >>blacklist.txt
+echo "$domain added to blacklist.txt"
+
+./compile.sh
 
 git diff
 
